b06e0cdaef1787f0e256340ed936b864d1a4fe3c
Intern-666 <666@automattor.com>
2026-08-18 09:03:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ st.markdown(
             font-size: 2.5rem !important;
             font-weight: 800 !important;
             letter-spacing: -0.5px;
-            line-height: 5;
+            line-height: 4;
             margin: 0 !important;
         }
 
